f37886ee6e3b3dd2d9c75b1448312d9142344251
Parley Martins <parleypachecomartins@gmail.com>
2016-09-02 14:47:07 -0300

    Add pictures

--- base.py
+++ base.py
@@ -8,32 +8,27 @@ def detect(frame):
     height, width, depth = frame.shape
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.equalizeHist(grayscale, grayscale)
-    classifier = cv2.CascadeClassifier("/home/parley/src/das/Detector_XML/haarcascade_frontalface_alt.xml")
+    classifier = cv2.CascadeClassifier("Detector_XML/haarcascade_frontalface_alt.xml")
     DOWNSCALE = 4
-    minisize = (frame.shape[1]/DOWNSCALE, frame.shape[0]/DOWNSCALE)
+    minisize = (width/DOWNSCALE, height/DOWNSCALE)
     miniframe = cv2.resize(frame, minisize)
     faces = classifier.detectMultiScale(miniframe)
     if len(faces) > 0:
         print("Face detected!") # Printing in the console
         for face in faces:
+            print(face)
             x, y, w, h = [v*DOWNSCALE for v in face]
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 25, 5, 0))
     return frame
 
 cap = cv2.VideoCapture(0)
 
-# while(True):
 ret, frame = cap.read()
-img = frame.copy()
 frame = detect(frame)
-
-cv2.imshow('frame', frame) #frame title
-    # if(cv2.waitKey(1) & 0xFF == ord('q')):
-    #     break
 
 cap.release()
 cv2.destroyAllWindows()
 
 plt.imshow(frame[:,:,::-1])
-plt.title('VAmos ver o que da nessa droga') #Image title
+plt.title('Faces recognized') #Image title
 plt.axis('off')
